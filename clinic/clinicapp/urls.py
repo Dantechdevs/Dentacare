a2ece7ca8ab18c9d4f21d79139b9921104002091
Daniel Ngwasi <damnngwasi@gmail.com>
2025-05-09 09:15:32 +0300

    added doctor

--- clinic/clinicapp/urls.py
+++ clinic/clinicapp/urls.py
@@ -4,4 +4,6 @@ from . import views
         
 urlpatterns = [
     path('', views.home, name='home'),
+    path('doctors.html', views.doctors, name='doctors'),
+    path('add-doctor.html', views.add_doctor, name='add_doctor'),
 ]
--- clinic/clinicapp/views.py
+++ clinic/clinicapp/views.py
@@ -3,3 +3,9 @@ from django.shortcuts import render
 # Create your views here.
 def home(request):
     return render(request, 'home.html',{})
+
+def doctors(request):
+    return render(request, 'doctors.html',{})
+
+def add_doctor(request):
+    return render(request, 'add-doctor.html',{})    
